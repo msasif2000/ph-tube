910c27756f920004a400d1c0821057e84d3899d6
Asif <msasifiiuc@gmail.com>
2023-09-02 20:04:26 +0600

    80% done

--- tube.js
+++ tube.js
@@ -1,25 +1,37 @@
-const loadContent = async () => {
-    const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/1000`)
-    const data = await res.json()
+const loadContent = async (id='1000') => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${id}`)
+    const data = await res.json();
     //console.log(data.data);
     videos = data.data;
     displayContent(videos);
 }
+const verify = (verified) => {
+    if (verified) {
+        return `<img src="./images/verify.png" alt="" class="h-[18px] w-[18px]">`
+    }
+    else {
+       return '';
+    }
+}
 displayContent = videos => {
     console.log(videos);
+    document.getElementById('video-container').innerHTML = '';
     videos.forEach(video => {
         const videoCard = document.createElement('div');
-        videoCard.classList = 'card bg-base-100 p-4 shadow-xl';
+        videoCard.classList = 'card bg-base-100';
         videoCard.innerHTML = `
-        <figure><img src="${video.thumbnail}" alt="" /></figure>
-                    <div class="flex gap-1">
+        <figure><img src="${video.thumbnail}" alt="" class="h-[250px] rounded-xl p-1" /></figure>
+                    <div class="flex gap-4 mt-4 p-2">
                         <div>
-                            <img src="${video.authors[0]}" alt="">
+                            <img src="${video.authors[0].profile_picture}" alt="" class="rounded-full h-[36px] w-[36px]">
                         </div>
                         <div>
-                            <h4 class="card-title">${video.title}</h4>
-                            <p>${video.authors[1]}</p>
-                            <p>${video.others.views}</p>
+                            <h4 class="card-title text-[16px] font-bold">${video.title}</h4>
+                            <div class="flex gap-2 items-center">
+                                <p>${video.authors[0].profile_name}</p> 
+                                <p>${verify(video.authors[0].verified)}</p>
+                            </div>
+                            <p>${video.others.views} views</p>
                         </div>
                     </div>
                     `;
@@ -28,16 +40,31 @@ displayContent = videos => {
     })
 }
 
-// const allHandle()=>{
-
+const allHandle = ()=>{
+    loadContent('1000');
+}
+const musicHandle = ()=>{
+    loadContent('1001');
+}
+const comedyHandle = () =>{
+    loadContent('1003');
+}
+const drawingHandle = () =>{
+    loadContent('1005');
+}
+// const sortByTime = () => {
+//     videos.sort((a, b) => {
+//         return b.others.posted_date - a.others.posted_date;
+//     })
+//     displayContent(videos);
 // }
 loadContent();
 
-// { "category_id": "1001", 
-// "thumbnail": "https://i.ibb.co/hdtZYbB/enchnting.jpg", 
-// "title": "Enchanted Harmonies", 
+// { "category_id": "1001",
+// "thumbnail": "https://i.ibb.co/hdtZYbB/enchnting.jpg",
+// "title": "Enchanted Harmonies",
 // "authors":
-//  [{ "profile_picture": "https://i.ibb.co/jh1q2F3/shopia.jpg", 
-//  "profile_name": "Sophia Williams", 
-//  "verified": false }], 
+//  [{ "profile_picture": "https://i.ibb.co/jh1q2F3/shopia.jpg",
+//  "profile_name": "Sophia Williams",
+//  "verified": false }],
 //  "others": { "views": "7.6K", "posted_date": "16450" } }]}
